025f8a36df48034883c5a5fdc7f6d9962f197c17
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-12-17 10:03:34 +0200

    Http: HTML parser

---
+++ lesson1/01-HTTP/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _01_HTTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Url = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.HTML = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Url
+            // 
+            this.Url.Location = new System.Drawing.Point(44, 12);
+            this.Url.Name = "Url";
+            this.Url.Size = new System.Drawing.Size(413, 20);
+            this.Url.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(463, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // HTML
+            // 
+            this.HTML.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.HTML.Location = new System.Drawing.Point(0, 50);
+            this.HTML.Name = "HTML";
+            this.HTML.Size = new System.Drawing.Size(550, 391);
+            this.HTML.TabIndex = 3;
+            this.HTML.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 441);
+            this.Controls.Add(this.HTML);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Url);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HTML Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Url;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox HTML;
+    }
+}
+
---
+++ lesson1/01-HTTP/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _01_HTTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(Url.Text);
+            request.Headers.Add("Accept-Language: ru-RU");
+
+            HttpWebResponse responce = null;
+            try
+            {
+                responce = (HttpWebResponse)request.GetResponse();
+                StreamReader reader = new StreamReader(responce.GetResponseStream(), Encoding.UTF8);
+                HTML.Text = reader.ReadToEnd();
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
